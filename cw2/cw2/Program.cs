5ba8dd864563fe8c7a0483d7edb006884d0209f6
Mateusz Bednarek <s18579@pjwstk.edu.pl>
2020-03-09 21:21:06 +0100

    v3 convertion to XML

--- cw2/cw2/Program.cs
+++ cw2/cw2/Program.cs
@@ -12,52 +12,91 @@ namespace cw2
     {
         static void Main(string[] args)
         {
-            var path = @"C:\Users\Mati\Desktop\cw2\dane.csv";
-            var lines = File.ReadLines(path);
-            var today = DateTime.Now;
-            HashSet<student> students = new HashSet<student>(new OwnComparator());
-            foreach (var line in lines)
+            try
             {
-                string[] student = line.Split(',');
-                if (student.Length == 9)
+                var path = @"C:\Users\Mati\Desktop\cw2\dane.csv";
+                var lines = File.ReadLines(args[0]);
+                var today = DateTime.Now;
+                HashSet<student> students = new HashSet<student>(new OwnComparator());
+                StreamWriter writer = new StreamWriter(@"C:\Users\Mati\Desktop\log.txt");
+
+                //cz.1 tworzenie HashSet i uzupelnianie nim danymi z pliku
+                foreach (var line in lines)
                 {
-                    if (!Array.Exists(student, ele => ele == ""))
+                    string[] student = line.Split(',');
+                    if (student.Length == 9)
                     {
-                        students.Add(new student
+                        if (!Array.Exists(student, ele => ele == ""))
                         {
-                            fname = student[0],
-                            lname = Regex.Replace(student[1], "[0-9]", ""),
-                            name = student[2],
-                            mode = student[3],
-                            indexNumber = "s" + student[4],
-                            birthdate = student[5],
-                            email = "s" + student[4] + @"@pjwstk.edu.pl",
-                            mothersname = student[7],
-                            fathersname = student[8]
-                        });
+                            students.Add(new student
+                            {
+                                fname = student[0],
+                                lname = Regex.Replace(student[1], "[0-9]", ""),
+                                name = student[2],
+                                mode = student[3],
+                                indexNumber = "s" + student[4],
+                                birthdate = student[5],
+                                email = "s" + student[4] + @"@pjwstk.edu.pl",
+                                mothersname = student[7],
+                                fathersname = student[8]
+                            });
+                        }
+                        else writer.WriteLine("Student o id: s" + student[4] + " ma puste pole");
                     }
-                    //else Console.WriteLine("Student o id: s" + student[4] + " ma puste pole");
+                    else writer.WriteLine("Student o id: s" + student[4] + " ma nie poprawna ilosc elementow");
                 }
-                //else Console.WriteLine("Student o id: s" + student[4] + " ma nie poprawna ilosc elementow");
+                writer.Close();
+
+                //cz.2 liczenie ilosci danych typow studiow
+                Dictionary<string, int> count = new Dictionary<string, int>(); // oblicza wystepowanie kazdych z typow studiow
+                foreach (var student in students)
+                {
+                    if (!count.ContainsKey(student.name)) count.Add(student.name, 1);
+                    else count[student.name]++;
+                }
+
+                //cz.3 konwersja na XML
+                if (args[2].Equals("xml"))
+                {
+                    XDocument doc = new XDocument(new XElement("uczelnia",
+                        new XAttribute("createdAt", today),
+                        new XAttribute("author", "Mateusz Bednarek"),
+                        new XElement("studenci",
+                            from student in students
+                            select new XElement("student",
+                            new XAttribute("indexNumber", student.indexNumber),
+                            new XElement("fname", student.fname),
+                            new XElement("lname", student.lname),
+                            new XElement("birthdate", student.birthdate),
+                            new XElement("email", student.email),
+                            new XElement("mothersName", student.mothersname),
+                            new XElement("fathersName", student.fathersname),
+                            new XElement("studies",
+                                new XElement("name", student.name),
+                                new XElement("mode", student.mode)
+                       ))),
+                        new XElement("activeStudies",
+                            from c in count
+                            select new XElement("studies",
+                                new XAttribute("name", c.Key),
+                                new XAttribute("numberOfStudents", c.Value)
+                                )
+                        )));
+                    doc.Save(args[1]);
+                }
+                else if (args[2].Equals("json")) Console.WriteLine("Jeszcze nie zaimplementowany JSON");
+                else Console.WriteLine("Wpisany zostal zly typ");
             }
-            XDocument doc = new XDocument(new XElement("uczelnia",
-                new XAttribute("createdAt",today),
-                new XAttribute("author","Mateusz Bednarek"),
-                new XElement("studenci",
-                    from student in students
-                    select new XElement("student",
-                    new XAttribute("indexNumber", student.indexNumber),
-                    new XElement("fname", student.fname),
-                    new XElement("lname", student.lname),
-                    new XElement("birthdate", student.birthdate),
-                    new XElement("email", student.email),
-                    new XElement("mothersName", student.mothersname),
-                    new XElement("fathersName", student.fathersname),
-                    new XElement("studies",
-                        new XElement("name", student.name),
-                        new XElement("mode", student.mode)
-               )))));
-            doc.Save(@"C:\Users\Mati\Desktop\data.xml");
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Podana scieszka jest niepoprawna");
+                throw;
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Plik nazwa nie istnieje");
+                throw;
+            }
         }
     }
 }
