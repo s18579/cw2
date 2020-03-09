6e166ecbe413075087de64f08dc2e1f00e79444d
s18579 <s18579@LAB001-13>
2020-03-09 09:58:11 +0100

    init

---
+++ cw2/OwnComparator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cw2
+{
+    class OwnComparator : IEqualityComparer<Student>
+    {
+        public bool Equals(Student x, Student y)
+        {
+            return StringComparer.InvariantCultureIgnoreCase.Equals($"{x.Imie} {x.Nazwisko} {x.Index}", $"{y.Imie} {y.Nazwisko} {y.Index}");
+        }
+
+        public int GetHashCode(Student obj)
+        {
+            return StringComparer.CurrentCultureIgnoreCase.GetHashCode($"{obj.Imie} {obj.Nazwisko} {obj.Index}");
+        }
+    }
+}
